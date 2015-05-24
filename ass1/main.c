23d64c438d282186704745c65d954ff092ebcfff
kradical <konrad_here@hotmail.com>
2015-05-23 21:09:17 -0700

    Updated main, see TODO

--- ass1/main.c
+++ ass1/main.c
@@ -19,75 +19,16 @@ typedef struct process{
     struct process *next;
 }PROCESS;
 
+void delete_process(pid_t pid);
+void add_process(PROCESS **head, pid_t pid, char **commands, int arguements);
+void print_process_list(PROCESS *head);
+
 void *try_malloc(int size);
 char *get_prompt();
 int parse_command(char *command, char ***p);
-void command_arbitrary(char **commandArray, int arguements, PROCESS **head);
-void command_change_directory(char **commandArray, int arguements);
-
-/*
-   delete_process(pid_t pid) 
-*/
-void delete_process(pid_t pid){
-    printf("DELETE");
-    return;
-}
-/*
-
-*/
-void add_process(PROCESS **head, pid_t pid, char **commands, int arguements){
-	int i = 0;	
-	if(*head == NULL){	
-	    (*head) = try_malloc(sizeof(PROCESS));
-		(*head)->pid = pid;
-		(*head)->arguements = arguements;
-		(*head)->processname = try_malloc(sizeof(commands[0]));
-		strcpy((*head)->processname, commands[0]);
-		(*head)->processargsv = try_malloc(sizeof(commands));		
-		for(i=0; i<arguements; i++){
-			(*head)->processargsv[i] = try_malloc(sizeof(commands[i]));
-			strcpy((*head)->processargsv[i], commands[i]);
-		}
-		(*head)->next = NULL;
-	}else{
-		PROCESS *temp = try_malloc(sizeof(PROCESS));
-		temp->pid = pid;
-		temp->arguements = arguements;
-		temp->next = NULL;
-		temp->processname = try_malloc(sizeof(commands[0]));
-		strcpy(temp->processname, commands[0]);
-		temp->processargsv = try_malloc(sizeof(commands));		
-		for(i=0; i<arguements; i++){
-			temp->processargsv[i] = try_malloc(sizeof(commands[i]));
-			strcpy(temp->processargsv[i], commands[i]);
-		}
-		PROCESS *p = *head;
-		while(p->next != NULL){
-			p=p->next;
-		}
-		p->next = temp;
-	}
-	return;
-}
-
-/*
-
-*/
-void print_process_list(PROCESS *head){
-	PROCESS *temp = head;
-	int i = 0;
-	int jobs = 0;
-	while(temp != NULL){
-		printf("%d:\t%s\t", temp->pid, temp->processname);
-		for(i=1; i < temp->arguements; i++){
-			printf("%s ", temp->processargsv[i]);
-		}		
-		printf("\n");
-		temp = temp->next;
-		jobs++;
-	}
-	printf("Total Background jobs:\t%d\n", jobs);
-}
+void command_arbitrary(char **command_array, int arguements, PROCESS **head);
+void command_change_directory(char **command_array, int arguements);
+void child_sig_handler(int signal_number)
 
 /*
     try_malloc, helpful function for mallocing char* with error checking
@@ -102,8 +43,74 @@ void *try_malloc(int size){
 }
 
 /*
+   delete_process
+*/
+void delete_process(pid_t pid){
+    printf("DELETE");
+    return;
+}
+
+/*
+   add_process
+*/
+void add_process(PROCESS **head, pid_t pid, char **commands, int arguements){
+    int i = 0;	
+    if(*head == NULL){	
+        (*head) = try_malloc(sizeof(PROCESS));
+        (*head)->pid = pid;
+        (*head)->arguements = arguements;
+        (*head)->processname = try_malloc(sizeof(commands[0]));
+        strcpy((*head)->processname, commands[0]);
+        (*head)->processargsv = try_malloc(sizeof(commands));		
+        for(i=0; i<arguements; i++){
+            (*head)->processargsv[i] = try_malloc(sizeof(commands[i]));
+            strcpy((*head)->processargsv[i], commands[i]);
+        }
+        (*head)->next = NULL;
+    }else{
+        PROCESS *temp = try_malloc(sizeof(PROCESS));
+        temp->pid = pid;
+        temp->arguements = arguements;
+        temp->next = NULL;
+        temp->processname = try_malloc(sizeof(commands[0]));
+        strcpy(temp->processname, commands[0]);
+        temp->processargsv = try_malloc(sizeof(commands));		
+        for(i=0; i<arguements; i++){
+            temp->processargsv[i] = try_malloc(sizeof(commands[i]));
+            strcpy(temp->processargsv[i], commands[i]);
+        }
+        PROCESS *p = *head;
+        while(p->next != NULL){
+        p=p->next;
+        }
+        p->next = temp;
+    }
+    return;
+}
+
+/*
+    print_process_list
+*/
+void print_process_list(PROCESS *head){
+    PROCESS *temp = head;
+    int i = 0;
+    int jobs = 0;
+    while(temp != NULL){
+        printf("%d:\t%s\t", temp->pid, temp->processname);
+        for(i=1; i < temp->arguements; i++){
+            printf("%s ", temp->processargsv[i]);
+        }		
+        printf("\n");
+        temp = temp->next;
+        jobs++;
+    }
+    printf("Total Background jobs:\t%d\n", jobs);
+}
+
+/*
     get_prompt creates a prompt for the RSI out of the current directory
-    and RSI: >
+    and RSI: "CWD" >. Dynamically allocates space for cwd doubling if size is
+    filled. Allows for use on file systems without PATH_MAX limits.
 */
 char *get_prompt(){
     int path_size = INITIAL_PATH_SIZE;
@@ -111,30 +118,31 @@ char *get_prompt(){
     char *prompt = NULL;
     char *prompt1 = "RSI: ";
     char *prompt2 = " >";
-    
+
     while(getcwd(current_directory, path_size) == NULL){
-            path_size *= 2;
-            current_directory = realloc(current_directory, sizeof(char) * path_size);
+        path_size *= 2;
+        current_directory = realloc(current_directory, sizeof(char) * path_size);
             
-            if(current_directory == NULL){
-                perror("Error allocating memory");
-                exit(1);
-            }
+        if(current_directory == NULL){
+            perror("Error allocating memory");
+            exit(1);
+        }
     }
     
     prompt = try_malloc(strlen(current_directory)+strlen(prompt1)+strlen(prompt2)+1);
     strcpy(prompt, prompt1);
     strcat(prompt, current_directory);
     strcat(prompt, prompt2);    
+
     free(current_directory);
-	current_directory = NULL;            
-    
-	return prompt;
+    current_directory = NULL;            
+
+    return prompt;
 }
 
 /*
     parse_command takes a string array and places each arguement into one
-    element. It does so doubling the buffer size when it fills up and 
+    element. It does so doubling the size when it fills up and 
     dynamically allocating space for each string which means every arguement
     can be as long as the user likes.
 */
@@ -143,15 +151,13 @@ int parse_command(char *command, char ***p){
     int command_array_size = INITIAL_COMMAND_BUFFER_SIZE;
     int arguements = 0;   
     char **command_array = try_malloc(sizeof(char*) * command_array_size);
-    
+
     token = strtok(command, " \t\n");
-     
-    while(token != NULL){
-        
+
+    while(token != NULL){  
         if(arguements == command_array_size - 1){
             command_array_size *= 2;
             command_array = realloc(command_array, sizeof(char*) * command_array_size);
-            printf("%d\n", command_array_size);
             if(command_array == NULL){
                 perror("Error allocating memory");
                 exit(1);
@@ -174,50 +180,45 @@ int parse_command(char *command, char ***p){
     for a binary with the same name. If fork() or execv() has an error it will 
     print to stderr.
 */
-void command_arbitrary(char **commandArray, int arguements, PROCESS **head){       
-    int status;
-	int eresult = 0;
+void command_arbitrary(char **command_array, int arguements, PROCESS **head){
     char **commands;
     int background = 0;
-	if(strcmp(commandArray[0], "bg") == 0){
-        commands = commandArray + 1;
+    if(strcmp(command_array[0], "bg") == 0){
+        commands = command_array + 1;
         background = 1;   
-    }else if(strcmp(commandArray[0], "bglist") == 0){
+    }else if(strcmp(command_array[0], "bglist") == 0){
         print_process_list(*head);
         return;
     }else{
-        commands = commandArray;
+        commands = command_array;
         background = 0;
     }    
     pid_t pid = fork();    
     if(pid == 0){
-	if(execvp(commands[0], commands) == -1){
-            perror("Error on execv");
-		if(background == 1)		
-		exit(1);
-        }
+    if(execvp(commands[0], commands) == -1){
+            perror("Error on execv");	
+            exit(1);
     }else if(pid < 0){
         perror("Failed to fork process");
     }else if(background == 1){
-		printf("PID %d", pid);
         add_process(head, pid, commands, arguements-1);   
-	}
+    }
 	if(background == 0){
-    	    wait(&status);
-	}
+        wait(NULL);
+    }
     return;
 }
 
 /*
-    command_change_directory gives errors for more than 2 arguements, invalid 
-    path and invalid HOME variable. Otherwise works on (cd ~ cd . cd .. cd ). 
+    command_change_directory prints errors for more than 2 arguements, invalid 
+    path and invalid HOME variable. Uses chdir() to change cwd
 */
-void command_change_directory(char **commandArray, int arguements){
+void command_change_directory(char **command_array, int arguements){
     if(arguements > 2){
         fprintf(stderr, "cd:    usage:  cd [dir]\n");
     }else if(arguements == 2){
-        if(strcmp(commandArray[1], "~") == 0){
-            char *home = getenv("HOME");            
+        if(strcmp(command_array[1], "~") == 0){
+            char *home = getenv("HOME");
 
             if(chdir(home) == 0){
                 return;
@@ -225,15 +226,14 @@ void command_change_directory(char **commandArray, int arguements){
                 fprintf(stderr, "Could not change directory HOME environment variable error\n");
             }
         }else{           
-            if(chdir(commandArray[1]) == 0){
+            if(chdir(command_array[1]) == 0){
                 return;
             }else{
                 fprintf(stderr, "Could not change directory invalid path\n");
             }
         }        
     }else if(arguements == 1){
-        char *home = getenv("HOME");            
-
+        char *home = getenv("HOME");
 	    if(chdir(home) == 0){
             return;
         }else{
@@ -244,23 +244,24 @@ void command_change_directory(char **commandArray, int arguements){
 }
 
 /*
-
+    child_sig_handler asynchronously handles child process termination and
+    deletes the process from the background process list.
 */
-void clean_up_child_process(int signal_number){
-	pid_t pid;	
-	while((pid = waitpid((pid_t)(-1), NULL, WNOHANG)) > 0){
-		printf("pid = %d\n", pid);
-	}
+void child_sig_handler(int signal_number){
+    pid_t pid;	
+    while((pid = waitpid((pid_t)(-1), NULL, WNOHANG)) > 0){
+        printf("pid = %d\n", pid);
+    }
 }
 
 int main(void){
-	/*setup signal handler*/
-	struct sigaction sigchld_action; 
-  	memset (&sigchld_action, 0, sizeof (sigchld_action)); 
- 	sigchld_action.sa_handler = &clean_up_child_process; 
-  	sigaction (SIGCHLD, &sigchld_action, NULL); 
+    /*setup signal handler*/
+    struct sigaction sigchld_action; 
+    memset (&sigchld_action, 0, sizeof (sigchld_action)); 
+    sigchld_action.sa_handler = &child_sig_handler; 
+    sigaction (SIGCHLD, &sigchld_action, NULL); 
     
-	int active = 1;
+    int active = 1;
     int i = 0;  
     int arguements = 0;  
     char *command = NULL;
@@ -270,36 +271,34 @@ int main(void){
     while(active){
         prompt = get_prompt();
         command = readline(prompt);
-		
-		free(prompt);
-		prompt = NULL;        
-		
-		if(strcmp(command, "quit") == 0){
-			printf("RSI: exit success");			
-			active = 0;
-			break;
-		}  
+
+        free(prompt);
+        prompt = NULL;        
+
+        if(strcmp(command, "quit") == 0){
+            printf("RSI: exit success");			
+            active = 0;
+            break;
+        }  
         arguements = parse_command(command, &command_array);
-		
+
         free(command);
         command = NULL;
-        
+
         if(strcmp(command_array[0], "cd") == 0){
             command_change_directory(command_array, arguements);
         }else{
             command_arbitrary(command_array, arguements, &head);
         }
 
-		for(i=0; i<arguements; i++){
+        for(i=0; i<arguements; i++){
             free(command_array[i]);
             command_array[i] = NULL;
         }
         free(command_array);
         command_array = NULL;
-    }
-    
+    }    
     printf("\n");
     return 0;
 }
-
 //TODO FREE LL on program exit, signal handler, give a user a message that child terminated and remove from linked list (including free)
