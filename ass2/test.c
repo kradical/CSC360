8bd4d6246e546e4962bc53019e8d7956e3ec073e
kradical <konrad_here@hotmail.com>
2015-06-02 12:50:30 -0700

    threads

--- ass2/test.c
+++ ass2/test.c
@@ -1,7 +1,24 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void *PrintHello(void *threadid){
+	long tid;
+	tid = (long)threadid;
+	printf("Hello World! It's thread #%ld!\n", tid);
+	pthread_exit(NULL);
+}
 
 int main(void){
-	printf("THREADSSS\n");
-	return 0;
+	pthread_t threads[5];
+	int rc;
+	long t;
+	for(t=0; t<5; t++){
+		rc = pthread_create(&threads[t], NULL, PrintHello, (void*)t);
+		if (rc){
+			printf("ERROR");
+			exit(-1);
+		}
+	}
+	pthread_exit(NULL);
 }
