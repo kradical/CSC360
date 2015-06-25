3002bd430781db9df2d1e76bdc8d015012cb5490
kradical <schultzk@uvic.ca>
2015-06-24 17:08:38 -0700

    cleaned up compiler warnings

--- ass2/p2/main.c
+++ ass2/p2/main.c
@@ -6,19 +6,19 @@
 #include <unistd.h>
 
 struct train{
-    pthread_t thread; //thread id
+    pthread_t thread; /* thread id */
     struct train *next;
-    char number; //id in range [0, 99]
-    char *direction; //East or West
-    char loading_time; //10ths of seconds in range [1, 99]
-    char crossing_time; //10ths of seconds in range [1, 99]
-    char priority; //0 for low priority 1 for high priority
+    char number; /* id in range [0, 99] */
+    char direction; /* e or w */
+    char loading_time; /* 10ths of seconds in range [1, 99] */
+    char crossing_time; /* 10ths of seconds in range [1, 99] */
+    char priority; /* 0 for low priority 1 for high priority */
 };
 
 void *try_malloc(int);
 struct timespec diff(struct timespec);
 void calculate_time(struct timespec, long *, int *, int *, int *);
-struct train *pop_train(char *direction);
+struct train *pop_train(char);
 void add_in_place(struct train *, struct train *);
 void add_train(struct train *);
 void *TrainFunction(void *);
@@ -70,9 +70,9 @@ void calculate_time(struct timespec ts_current, long *msec, int *sec, int *min, 
 /* dispatcher uses this function to get the head of the list, direction is
    passed to ensure the proper list, both mutexes are locked in the
    dispatcher and unlocked here */
-struct train *pop_train(char *direction){
+struct train *pop_train(char direction){
     struct train *temp;
-    if(direction == "West"){
+    if(direction == 'w'){
         pthread_mutex_unlock(&eastbound_lock);
         temp = westbound_head;
         westbound_head = temp->next;
@@ -89,11 +89,11 @@ struct train *pop_train(char *direction){
 /* helper function to add a train where it's supposed to go in it's list */
 void add_in_place(struct train *prev, struct train *t){
     struct train * temp;
-    if(prev == NULL && t->direction == "West"){
+    if(prev == NULL && t->direction == 'w'){
         temp = westbound_head;
         westbound_head = t;
         t->next = temp;
-    }else if(prev == NULL && t->direction == "East"){
+    }else if(prev == NULL && t->direction == 'e'){
         temp = eastbound_head;
         eastbound_head = t;
         t->next = temp;
@@ -108,14 +108,14 @@ void add_in_place(struct train *prev, struct train *t){
    priority > load time > number 
     1 > 0      asc.        asc. */
 void add_train(struct train *t){
-    if(t->direction == "West" && westbound_head == NULL){
+    if(t->direction == 'w' && westbound_head == NULL){
         westbound_head = t;
-    }else if(t->direction == "East" && eastbound_head == NULL){
+    }else if(t->direction == 'e' && eastbound_head == NULL){
         eastbound_head = t;
     }else{
         struct train *temp = NULL;
         struct train *prev = NULL;
-        if(t->direction == "West"){
+        if(t->direction == 'w'){
             temp = westbound_head;
         }else{
             temp = eastbound_head;
@@ -146,48 +146,43 @@ void add_train(struct train *t){
    barrier, sleeps for loading time, adds itself to the proper list then 
    signals the dispatcher */
 void *TrainFunction(void *trainid){
-    int return_code;
-    struct train *t;
-    t = (struct train *)trainid;
+    struct train *t = (struct train *)trainid;
     struct timespec ts_current;
     long msec;
     int sec;
     int min;
     int hour;
-
+    
     pthread_barrier_wait (&initial_barrier);
-    return_code = usleep(t->loading_time*100000);
-    if(return_code){
+    if(usleep(t->loading_time*100000)){
         perror("ERROR: return code from usleep() is -1");
         exit(1);
     } 
-    return_code = clock_gettime(CLOCK_MONOTONIC, &ts_current);
-    if(return_code){
+    if(clock_gettime(CLOCK_MONOTONIC, &ts_current)){
         perror("ERROR: return code from clock_gettime() is -1");
         exit(1);
     } 
     calculate_time(ts_current, &msec, &sec, &min, &hour);
-    if(t->direction == "West"){
+    if(t->direction == 'w'){
         while(pthread_mutex_lock(&westbound_lock) != 0);
         add_train(t);
-        printf("%02d:%02d:%02d.%1ld Train %2d is ready to go %4s\n", hour, min, sec, msec, t->number, t->direction);
         pthread_mutex_unlock(&westbound_lock);
     }else{
         while(pthread_mutex_lock(&eastbound_lock) != 0);
         add_train(t);
-        printf("%02d:%02d:%02d.%1ld Train %2d is ready to go %4s\n", hour, min, sec, msec, t->number, t->direction);
         pthread_mutex_unlock(&eastbound_lock);
     }
+    printf("%02d:%02d:%02d.%1ld Train %2d is ready to go %4s\n", hour, min, sec, msec, t->number, t->direction == 'w' ? "West":"East");
     pthread_cond_signal(&added_cv);
+    pthread_exit(NULL);
 }
 
 /* function called by main after spawning all the train threads, ensures the 
    trains are sent in the right order, keeps count to ensure all the trains
    are sent */
 void dispatcher(int numtrains){
-    int return_code;
     int trains_finished = 0;
-    char *prev_direction = "East";
+    char prev_direction = 'e';
     struct train *temp;
     struct timespec ts_current;
     long msec;
@@ -197,7 +192,7 @@ void dispatcher(int numtrains){
 
     while(trains_finished < numtrains){
         while(pthread_mutex_lock(&main_track_lock) != 0);
-        while(westbound_head == NULL && eastbound_head == NULL){ //wait if lists are both empty
+        while(westbound_head == NULL && eastbound_head == NULL){ /* wait if lists are both empty */
             pthread_cond_wait(&added_cv, &main_track_lock);
         }
         pthread_mutex_unlock(&main_track_lock);
@@ -206,50 +201,48 @@ void dispatcher(int numtrains){
         if(westbound_head != NULL && eastbound_head != NULL){
             if(westbound_head->priority == 1){
                 if(eastbound_head->priority == 1){
-                    if(prev_direction == "East"){ //both high priority last train east
-                        temp = pop_train("West");
-                    }else{ //both high priority last train west
-                        temp = pop_train("East");
+                    if(prev_direction == 'e'){ /* both high priority last train east */
+                        temp = pop_train('w');
+                    }else{ /* both high priority last train west */
+                        temp = pop_train('e');
                     }
-                }else{ //west high priority east low priority
-                    temp = pop_train("West");
+                }else{ /* west high priority east low priority */
+                    temp = pop_train('w');
                 }
             }else{
                 if(eastbound_head->priority == 0){
-                    if(prev_direction == "East"){ //both low priority last train east
-                        temp = pop_train("West");
-                    }else{ //both low priority last train west
-                        temp = pop_train("East");
+                    if(prev_direction == 'e'){ /* both low priority last train east */
+                        temp = pop_train('w');
+                    }else{ /* both low priority last train west */
+                        temp = pop_train('e');
                     }
-                }else{ //east high priority west low priority
-                    temp = pop_train("East");
+                }else{ /* east high priority west low priority */
+                    temp = pop_train('e');
                 }
             }
-        }else if(westbound_head != NULL){ //only west trains
-            temp = pop_train("West");
-        }else{ //only east trains
-            temp = pop_train("East");
+        }else if(westbound_head != NULL){ /* only west trains */
+            temp = pop_train('w');
+        }else{ /* only east trains */
+            temp = pop_train('e');
         }
         
         prev_direction = temp->direction;
         
-        clock_gettime(CLOCK_MONOTONIC, &ts_current);
-        if(return_code){
+        if(clock_gettime(CLOCK_MONOTONIC, &ts_current)){
             perror("ERROR: return code from clock_gettime() is -1");
             exit(1);
         } 
         calculate_time(ts_current, &msec, &sec, &min, &hour);
-        printf("%02d:%02d:%02d.%1ld Train %2d is ON the main track going %s\n", hour, min, sec, msec, temp->number, temp->direction);
+        printf("%02d:%02d:%02d.%1ld Train %2d is ON the main track going %4s\n", hour, min, sec, msec, temp->number, temp->direction == 'w' ? "West":"East");
         
         usleep(temp->crossing_time*100000);
         
-        clock_gettime(CLOCK_MONOTONIC, &ts_current);
-        if(return_code){
+        if(clock_gettime(CLOCK_MONOTONIC, &ts_current)){
             perror("ERROR: return code from clock_gettime() is -1");
             exit(1);
         } 
         calculate_time(ts_current, &msec, &sec, &min, &hour);
-        printf("%02d:%02d:%02d.%1ld Train %2d is OFF the main track after going %s\n", hour, min, sec, msec, temp->number, temp->direction);
+        printf("%02d:%02d:%02d.%1ld Train %2d is OFF the main track after going %4s\n", hour, min, sec, msec, temp->number, temp->direction == 'w' ? "West":"East");
         
         trains_finished++;
     }
@@ -269,15 +262,15 @@ void parse_input_file(FILE *fp, struct train *trains, int numtrains){
         trains[i].loading_time = (char)loading_buf;
         trains[i].crossing_time = (char)crossing_buf;
         trains[i].number = (char)i;
-        if(direction_buf >= 'a'){ //lowercase
+        if(direction_buf >= 'a'){ /* lowercase */
             trains[i].priority = 0;
-        }else{ //uppercase
+        }else{ /* uppercase */
             trains[i].priority = 1;
         }
         if(direction_buf == 'W' || direction_buf == 'w'){           
-            trains[i].direction = "West";
+            trains[i].direction = 'w';
         }else{
-            trains[i].direction = "East";
+            trains[i].direction = 'e';
         }
         i++;
     }
@@ -288,22 +281,23 @@ void parse_input_file(FILE *fp, struct train *trains, int numtrains){
 }
 
 int main(int argc, char *argv[]){
+    int return_code;
+    int i = 0;
+    int number_of_trains;
+    struct train *temp = NULL;
+    struct train * trains = NULL;
+    FILE *input_file = NULL;
     if( argc != 3 ){
         fprintf(stderr, "Usage: %s [FILE] [INT]\n", argv[0]);
         exit(1);
     }
     
-    int number_of_trains = atoi(argv[2]);
+    number_of_trains = atoi(argv[2]);
     if(number_of_trains < 1){
         fprintf(stderr, "Usage: %s [FILE] [INT]\nOnly use numbers in [1, %d]\n", argv[0], INT_MAX);
         exit(1);
     }
-    
-    int return_code;
-    int i = 0;
-    struct train *temp;
-    struct train *trains = try_malloc(sizeof(struct train)*number_of_trains);
-    FILE *input_file = NULL;
+    trains = try_malloc(sizeof(struct train)*number_of_trains);
 
     input_file = fopen(argv[1], "r");
     if(input_file == NULL){
@@ -348,8 +342,7 @@ int main(int argc, char *argv[]){
             exit(1);
         }
     }
-    return_code = clock_gettime(CLOCK_MONOTONIC, &ts_start);
-    if(return_code){
+    if(clock_gettime(CLOCK_MONOTONIC, &ts_start)){
         perror("ERROR: return code from clock_gettime() is -1");
         exit(1);
     } 
