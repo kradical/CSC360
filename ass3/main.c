f1b358c2d29444a83f6d314204729d08d475e4ee
kradical <schultzk@uvic.ca>
2015-07-19 15:52:41 -0700

    part 3 working, test after inserting subdirs

--- ass3/main.c
+++ ass3/main.c
@@ -10,7 +10,8 @@
 #include <sys/stat.h> 
 #include <time.h> 
 #include <unistd.h> 
- 
+
+#define FILENAMELIM 31
 
 struct superblock_t{
     uint16_t block_size;
@@ -42,6 +43,68 @@ uint8_t onebfield(char *fp, int ndx){
     return fp[ndx]&0xFF;
 }
 
+#if defined(PART4)
+void putFile(char *ifile, char *olocation, char *fp){
+    printf("%s\n", ifile);
+}
+#endif
+
+#if defined(PART3)
+void transferFile(char *fp, char *filename, uint32_t filesize, int start){
+    FILE *new;
+    new = fopen(filename, "wb");
+    fwrite(fp+start, filesize, 1, new);
+}
+
+int filenameMatch(char *fp, char *subdirname, int ndx){
+    int x;
+    for(x=0; x<FILENAMELIM; x++){
+        if(subdirname[x] != fp[ndx+27+x]){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void getFile(char* dirname, char* fp, int start, int end){
+    if(dirname[0] == '/'){
+        dirname++;
+        if(dirname[0] == '\0'){
+            fprintf(stderr, "Input format: /subdir/subdir/filename\n");
+            return;
+        }
+        int i = 0;
+        char tempbuf[FILENAMELIM];
+        while(dirname[0]  != '/' && dirname[0] != '\0'){
+            tempbuf[i++] = dirname[0];
+            dirname++;
+        }
+        while(i<FILENAMELIM){
+            tempbuf[i++] = 0;
+        }
+        for(i=start; i<end; i+=64){
+            if(filenameMatch(fp, tempbuf, i)){
+                uint32_t startb = fourbfield(fp, i+1);
+                uint32_t dirsizeb = fourbfield(fp, i+5);
+                if(dirname[0] == '\0'){
+                    uint32_t filesize = fourbfield(fp, i+9);
+                    if((fp[i]&3) == 3) transferFile(fp, tempbuf, filesize, startb*SB.block_size);
+                    else fprintf(stderr, "File not found.\n");
+                }else{
+                    getFile(dirname, fp, startb*SB.block_size, (startb+dirsizeb)*SB.block_size);
+                }
+                return;
+            }
+        }
+        fprintf(stderr, "File not found.\n");
+    }else{
+        fprintf(stderr, "Input format: /subdir/subdir/filename\n");
+        exit(1);
+    }
+
+}
+#endif
+
 #if defined(PART2)
 void printFileInfo(char* fp, int start, int end){
     int i;
@@ -54,9 +117,9 @@ void printFileInfo(char* fp, int start, int end){
             continue;
         }
         uint32_t filesize = fourbfield(fp, i+9);
-        char filename[31];
+        char filename[FILENAMELIM];
         int x = 0;
-        for(x=0; x<31; x++){
+        for(x=0; x<FILENAMELIM; x++){
             filename[x] = fp[i+27+x];
         }
         uint16_t year = twobfield(fp, i+20);
@@ -70,10 +133,10 @@ void printFileInfo(char* fp, int start, int end){
     }
 }
 
-int filenameMatch(char *fp, uint8_t *subdirname, int ndx){
+int dirNameMatch(char *fp, char *subdirname, int ndx){
     if((fp[ndx] & 5) != 5) return 0;
     int x = 0;
-    for(x=0; x<31; x++){
+    for(x=0; x<FILENAMELIM; x++){
         if(subdirname[x] != fp[ndx+27+x]){
             return 0;
         }
@@ -89,18 +152,18 @@ void printDirInfo(char* dirname, char* fp, int start, int end){
             return;
         }
         int i = 0;
-        uint8_t tempbuf[31];
+        char tempbuf[FILENAMELIM];
         while(dirname[0]  != '/' && dirname[0] != '\0'){
             tempbuf[i++] = dirname[0];
             dirname++;
         }
-        while(i<31){
+        while(i<FILENAMELIM){
             tempbuf[i++] = 0;
         }
         for(i=start; i<end; i+=64){
-            if(filenameMatch(fp, tempbuf, i)){
+            if(dirNameMatch(fp, tempbuf, i)){
                 uint32_t startb = fourbfield(fp, i+1);
-                uint32_t dirsizeb = fourbfield(fp, i+9);
+                uint32_t dirsizeb = fourbfield(fp, i+5);
                 if(dirname[0] == '\0'){
                     printFileInfo(fp, startb*SB.block_size, (startb+dirsizeb)*SB.block_size);
                 }else{
@@ -109,9 +172,9 @@ void printDirInfo(char* dirname, char* fp, int start, int end){
                 return;
             }
         }
-        fprintf(stderr, "Could not locate directory.\n");
+        fprintf(stderr, "Directory not found.\n");
     }else{
-        fprintf(stderr, "Specify directory of format: /subdir/subdir/subdir\n");
+        fprintf(stderr, "Input format: /subdir/subdir/subdir\n");
         exit(1);
     }
 
@@ -188,14 +251,11 @@ int main(int argc, char* argv[]){
         if(argc == 3) printDirInfo(argv[2], p, SB.rootstart*SB.block_size, (SB.rootstart+SB.root_block_count)*SB.block_size);
         else fprintf(stderr, "USAGE: ./disklist [disk img] [directory]\n");
     #elif defined(PART3)
-        // int i;
-        // for(i=0;i<512*6400;i+=1){
-        //     if((unsigned long)p[i] > 255){
-        //         printf("%d %.2X\n", i, p[i]);
-        //     }
-        // }
+        if(argc == 3) getFile(argv[2], p, SB.rootstart*SB.block_size, (SB.rootstart+SB.root_block_count)*SB.block_size);
+        else fprintf(stderr, "USAGE: ./diskget [disk img] [filename]\n");
     #elif defined(PART4)
-    printf("Part 4: diskput\n");
+        if(argc == 4) putFile(argv[2], argv[3], p);
+        else fprintf(stderr, "USAGE: ./diskput [disk img] [local filename] [disk directory]\n");
     #endif
 
     return 0;
